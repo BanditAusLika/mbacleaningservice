56df800f7da89dd39baba9279349a177e6c56d1c
Srdan Radenovic <86351821+BanditAusLika@users.noreply.github.com>
2025-05-27 14:28:13 +0200

    Update Footer.js
    Updated footer year to 2025

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2024 by MBA Cleaning and Vegetation Services Pty Ltd.</p>
+      <p>© 2025 by MBA Cleaning and Vegetation Services Pty Ltd.</p>
       <a href="https://mftgworks.org" target="_blank" rel="noopener noreferrer">
         Visit MFTG
       </a>
