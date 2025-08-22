10241f5104445d260f11366dc9b580ebdf17bee4
Srdan Radenovic <ai.de.f2020@gmail.com>
2025-08-22 19:00:26 +0200

    Update Gallery and deploy

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,18 +5,24 @@ const Gallery = () => {
   const [lightboxItem, setLightboxItem] = useState(null);
 
   const images = [
-    '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg',
-    '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg',
-    '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg',
-    '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg',
-    '29.jpg', '30.jpg', '31.jpg', '32.jpg', '33.jpg', '34.jpg', '35.jpg',
-    '36.jpg', '37.jpg', '38.jpg', '39.jpg', '40.jpg', '41.jpg', '42.jpg',
-    '43.jpg', '44.jpg', '45.jpg', '46.jpg', '47.jpg', '48.jpg', '49.jpg',
-    '50.jpg', '51.jpg', '52.jpg', '53.jpg', '54.jpg', '55.jpg', '56.jpg',
-    '57.jpg', 'veg1.jpg', 'veg2.jpg', 'veg3.jpg', 'veg4.jpg'
+    '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg',
+    '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg',
+    '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg',
+    '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg',
+    '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg',
+    '26.jpg', '27.jpg', '28.jpg', '29.jpg', '30.jpg',
+    '31.jpg', '32.jpg', '33.jpg', '34.jpg', '35.jpg',
+    '36.jpg', '37.jpg', '38.jpg', '39.jpg', '40.jpg',
+    '41.jpg', '42.jpg', '43.jpg', '44.jpg', '45.jpg',
+    '46.jpg', '47.jpg', '48.jpg', '49.jpg', '50.jpg',
+    '51.jpg', '52.jpg', '53.jpg', '54.jpg', '55.jpg',
+    '56.jpg', '57.jpg', 'veg1.jpg', 'veg2.jpg',
+    'veg3.jpg', 'veg4.jpg'
   ];
 
-  const videos = ['vid1.mp4', 'vid2.mp4', 'vid3.mp4', 'vid4.mp4'];
+  const videos = [
+    'vid1.mp4', 'vid2.mp4', 'vid3.mp4', 'vid4.mp4'
+  ];
 
   const openLightbox = (item, type) => {
     setLightboxItem({ item, type });
@@ -36,7 +42,7 @@ const Gallery = () => {
           <img
             key={`img-${index}`}
             src={`${process.env.PUBLIC_URL}/images/gallery/${src}`}
-            alt={`Gallery Image ${index + 1}`}
+            alt={`Gallery item ${index + 1}`}
             onClick={() => openLightbox(src, 'image')}
           />
         ))}
